60d3f0856e922e6dc1af87906d12449e232fd525
wtrinh02 <yelsew02@gmail.com>
2025-12-09 20:28:50 -0800

    Refactored by adding Factory Design Pattern

---
+++ src/main/java/com/studyplanner/Factory/TaskFactory.java
@@ -0,0 +1,33 @@
+package main.java.com.studyplanner.Factory;
+
+import com.studyplanner.models.Task;
+import java.time.LocalDateTime;
+
+/**
+ * Factory for creating different types of tasks.
+ */
+public class TaskFactory {
+    public static Task createTask(TaskType type, String title, LocalDateTime deadline, int effort) {
+        Task task = new Task(title, deadline, effort);
+        
+        // Add type-specific prefixes or behaviors
+        switch (type) {
+            case ASSIGNMENT:
+                task.setTitle("[Assignment] " + title);
+                break;
+            case EXAM:
+                task.setTitle("[Exam Prep] " + title);
+                break;
+            case PROJECT:
+                task.setTitle("[Project] " + title);
+                break;
+            case REVIEW:
+                task.setTitle("[Review] " + title);
+                break;
+            default:
+                break;
+        }
+        
+        return task;
+    }
+}
---
+++ src/main/java/com/studyplanner/Factory/TaskType.java
@@ -0,0 +1,11 @@
+package main.java.com.studyplanner.Factory;
+
+/**
+ * Enum representing different types of tasks.
+ */
+public enum TaskType {
+    ASSIGNMENT,
+    EXAM,
+    PROJECT,
+    REVIEW
+}
